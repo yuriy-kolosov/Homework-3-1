9e855a3815f5db772369b8e65a8d6ed29bf95c30
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-07-01 00:29:14 +0300

    Домашнее задание (3)(4). Управление схемами баз данных. Задача 4-2, редакция 2

--- scripts422.sql
+++ scripts422.sql
@@ -1,28 +1,32 @@
 -- Курс 4 - урок 2 - задание 2
 -- Проект school hogwarts
 -- База данных garage
--- Таблица 1
+-- Таблица 1 (водитель)
 CREATE TABLE driver (
-    id int4 NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,  -- автоинкрементируемый идентификатор
     name varchar(50) NOT NULL,
-    age int4 CHECK (age>0),
+    age int4 CHECK (age > 0),
     license boolean DEFAULT false
-    )
--- Таблица 2
+    );
+-- Таблица 2 (тип машины, - бренд и модель вместе образуют уникальную комбинацию)
 CREATE TABLE car_type (
     brand varchar(50) UNIQUE NOT NULL,
     model varchar(50) UNIQUE NOT NULL,
-    price float4 CHECK (price>0),
+    price NUMERIC(10, 2) CHECK (price > 0),
     PRIMARY KEY (brand,model)
-    )
--- Таблица 3
+    );
+-- Таблица 3 (машина, - с указанием уникального номера, бренда и модели)
 CREATE TABLE car (
     number varchar(15) UNIQUE NOT NULL,
-    brand varchar(50) REFERENCES car_type (brand) ON DELETE CASCADE,
-    model varchar(50) REFERENCES car_type (model) ON DELETE CASCADE
-    )
--- Таблица 4
+    brand varchar(50) NOT NULL,
+    model varchar(50) NOT NULL,
+    FOREIGN KEY (brand, model) REFERENCES car_type(brand, model) ON DELETE CASCADE
+    );
+-- Таблица 4 (владелец машины, - много водителей могут иметь доступ к одной машине)
 CREATE TABLE car_owner (
-    car_number varchar(15) NOT NULL REFERENCES car (number) ON DELETE CASCADE,
-    driver_id int4 NOT NULL REFERENCES driver (id) ON DELETE CASCADE
-    )
+    car_number varchar(15) NOT NULL,
+    driver_id INT NOT NULL,
+    PRIMARY KEY (car_number, driver_id),
+    FOREIGN KEY (car_number) REFERENCES car(number) ON DELETE CASCADE,
+    FOREIGN KEY (driver_id) REFERENCES driver(id) ON DELETE CASCADE
+    );
--- scripts423.sql
+++ scripts423.sql
@@ -1,11 +1,12 @@
 -- Курс 4 - урок 2 - задание 3
 -- Проект school hogwarts
 -- База данных hogwarts
--- Запрос 1
+-- Запрос 1 (все студенты, - каждый студент принадлежит определённому факультету,
+--           идентификатор факультета в классе Faculty содержит аннотацию - @Column(name = "faculty_id") )
 SELECT s.name AS student_name, s.age AS student_age, f.name AS faculty_name
     FROM student AS s
-    LEFT JOIN faculty AS f ON s.faculty_id = f.faculty_id
--- Запрос 2
+    INNER JOIN faculty AS f ON s.faculty_id = f.faculty_id
+-- Запрос 2 (студенты, - у которых имеется аватарка)
 SELECT s.name AS student_name, s.age AS student_age
     FROM student AS s
     INNER JOIN avatar AS a ON s.id = a.student_id
